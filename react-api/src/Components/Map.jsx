94089ae6c9b17145ff88885250b1691e3c17e7d2
Hanna <mustonenhanna@icloud.com>
2024-09-16 13:33:02 +0200

    added map + connected api, added markers and popUp

--- react-api/src/Components/Map.jsx
+++ react-api/src/Components/Map.jsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+const icon = new L.Icon({
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+});
 
 export default function Map() {
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+    Axios.get("https://polisen.se/api/events").then((response) => {
+      const data = response.data;
+
+      const markers = data.map((marker) => {
+        const [lat, lon] = marker.location.gps.split(",").map(Number);
+        return {
+          id: marker.id,
+          position: [lat, lon],
+          popUp: marker.name,
+        };
+      });
+      setMarkers(markers);
+    });
+  }, []);
+
   return (
     <div className="map-section">
       <MapContainer
-        center={[48.8566, 2.3522]}
-        zoom={13}
+        center={[59.325, 18.05]}
+        zoom={5}
         className="leaflet-container"
       >
         <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        {markers.map((marker, index) => (
+          <Marker key={marker.id} position={marker.position} icon={icon}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
       </MapContainer>{" "}
     </div>
   );
