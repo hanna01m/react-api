94c0ff1b304c1799924605b793d4b76f74b5dfa0
Hanna <mustonenhanna@icloud.com>
2024-09-16 14:00:37 +0200

    added header component

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -2,14 +2,17 @@ import { useEffect, useState } from "react";
 import "./List.css";
 import "./Map.css";
 import "./App.css";
+import "./Header.css";
 
 import List from "./Components/List";
 import Map from "./Components/Map";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <>
       <div className="App">
+        <Header />
         <List />
         <Map />
       </div>
---
+++ react-api/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1 className="header-title">API POLISEN - SENASTE HÄNDELSER</h1>
+    </header>
+  );
+}
+
+export default Header;
