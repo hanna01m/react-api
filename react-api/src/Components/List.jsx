2d6b7a1da534900b11ee1e3e39aaef6adb32c788
Hanna <mustonenhanna@icloud.com>
2024-09-17 11:42:10 +0200

    responsive, css

--- react-api/src/Components/List.jsx
+++ react-api/src/Components/List.jsx
@@ -1,21 +1,38 @@
 import React, { useState, useEffect, useRef } from "react";
 import Axios from "axios";
 
+const dateTimeFormat = (datetime) => {
+  const date = new Date(datetime);
+
+  const dateFormat = { year: "numeric", month: "2-digit", day: "2-digit" };
+  const timeFormat = { hour: "2-digit", minute: "2-digit", hour12: false };
+
+  const formattedDate = date.toLocaleDateString("sv-SE", dateFormat); // YYYY-MM-DD
+  const formattedTime = date.toLocaleTimeString("sv-SE", timeFormat);
+
+  return `${formattedDate} ${formattedTime}`;
+};
+
 function List() {
   const [allData, setAllData] = useState([]);
-  const [showMore, setShowMore] = useState(10);
+  const [showMore, setShowMore] = useState(8);
   const scrollEndRef = useRef(null);
 
   const handleShowMore = () => {
-    setShowMore((prevCount) => prevCount + 5);
+    setShowMore((prevCount) => prevCount + 4);
 
     setTimeout(() => {
-      scrollEndRef.current.scrollIntoView({ behavior: "smooth" });
+      const tableContainer = scrollEndRef.current?.parentNode;
+      if (tableContainer) {
+        tableContainer.scrollTop = tableContainer.scrollHeight;
+      }
     }, 150);
   };
 
   useEffect(() => {
-    Axios.get("https://polisen.se/api/events").then((response) => {
+    Axios.get("https://polisen.se/api/events", {
+      timeout: 5000,
+    }).then((response) => {
       if (response.data.length > 0) {
         setAllData(response.data);
         console.log(response.data);
@@ -42,7 +59,7 @@ function List() {
                 <tr key={event.id}>
                   <td>{event.location.name}</td>
                   <td>{event.summary}</td>
-                  <td>{event.datetime}</td>
+                  <td>{dateTimeFormat(event.datetime)}</td>
                 </tr>
               ))}
             </tbody>
